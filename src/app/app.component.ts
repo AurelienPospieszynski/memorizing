fd0fcb75f85361910c88a4a995250cec96d75405
AurelienPospieszynski <aurelien.pos@hotmail.fr>
2024-01-17 23:30:29 +0100

    Add BannerComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { BannerComponent } from './components/Banner/banner/banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ButtonModule],
+  imports: [CommonModule, RouterOutlet, BannerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
